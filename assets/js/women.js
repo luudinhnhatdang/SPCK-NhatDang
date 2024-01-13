2c6ab8a7b1139ab1877271b9caed50d150bc319e
luudinhnhatdang <147293639+luudinhnhatdang@users.noreply.github.com>
2024-01-14 00:44:58 +0700

    dang len upd

--- assets/js/women.js
+++ assets/js/women.js
@@ -44,18 +44,76 @@ const dataWomen = [
 ];
 
 // LẤY THẺ DIV CHỨA CÁC SẢN PHẨM WOMEN BẰNG GETELEMENTBYID
-const womenOuter = document.getElementById("");
-
+const womenOuter = document.getElementById("womenOuter");
 // HÀM RENDERMEN ĐỂ THÊM TỪNG THẺ DIV VÀO THẺ WOMENOUTER BẰNG CÁCH DUYỆT DATA WOMEN TRONG VÒNG FOR
-function renderWomen() {}
+function renderWomen() {
+  womenOuter.innerHTML = null;
+  for (i of dataWonen) {
+    menOuter.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div data-aos="fade-up" class="shopping-card" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <div class="shoes-img-container">
+                      <img id="shoesImg" src="${i.image}" alt="">
+                      <a href="#" class="overlay"></a>
+                    </div>
+                    <div class="card-info">
+                      <h5 id="shoesName" class="shoes-name">${i.name}</h5>
+                      <p id="shoesPrice">$${i.price}</p>
+                      <div class="star">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                      </div>
+                    </div>
+                  </div>
+          `
+    );
+  }
+  searchnWomen();
+  clickWomenShoes();
+}
 
 // HÀM SEARCHMEN ĐỂ SEARCH GIÀY BẰNG TÊN CỦA ĐÔI GIÀY
-function searchWomen() {}
+function searchWomen() {
+  document.getElementById("searchInput").addEventListener("change", (e) => {
+    let searchInput = e.target.value.toUpperCase();
+    const card = document.querySelectorAll(".shopping-card");
+    let shoesName = document.querySelectorAll(".shoes-name");
+    shoesName.forEach((items, index) => {
+      if (items.innerText.toUpperCase().includes(searchInput)) {
+        card[index].classList.remove("hide");
+      } else {
+        card[index].classList.add("hide");
+      }
+    });
+  });
+}
 
 // HÀM CLICKMENSHOES ĐỂ HIỂN THỊ THÔNG TIN GIÀY KHI CLICK VÀO GIÀY
-function clickMenShoes() {}
-
+function clickMenShoes() {
+  const listWomenShoes = document.querySelectorAll(".shopping-card");
+  listWomenShoes.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      document.getElementById("womenImg").src = dataWomen[index].image;
+      document.getElementById("womenName").innerHTML = dataWomen[index].name;
+      document.getElementById("womenPrice").innerHTML =
+        dataMen[index].price + "$";
+      document.getElementById("womenDes").innerHTML = dataWomen[index].des;
+      document.getElementById("btnAdd").onclick = () => {
+        addToCart(index);
+      };
+    });
+  });
+}
 // HÀM ADDTOCART ĐỂ THÊM GIÀY VÀO LOCALSTORAGE
-function addToCart(index) {}
+function addToCart(index) {
+  const listCart = JSON.parse(localStorage.getItem("cart")) || [];
+  listCart.push(dataWomen[index]);
+  localStorage.setItem("cart", JSON.stringify(listCart));
+  alert("Add To Cart Successfully!");
+}
 
 window.onload = renderWomen();
